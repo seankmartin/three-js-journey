ba1faa1d2922794d8c729aa6f05bd5181b422de7
Sean Martin <martins7@tcd.ie>
2023-10-10 11:45:37 +0100

    Add files for 04

---
+++ 04-local-server/src/script.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene()
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+// Can also provide a hex value for color
+// E.g. 0xff0000 for red
+const material = new THREE.MeshBasicMaterial({ color: 'red' })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+const aspect_ratio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(75, aspect_ratio, 0.1, 100)
+camera.position.set(1.5, 0.5, 3)
+scene.add(camera)
+
+const canvas = document.querySelector('.webgl')
+// NOTE in JS you can remove the canvas: canvas if the variable name is the same
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
+console.log(renderer)
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Three JS journey
 
 Following the process of https://threejs-journey.com
+
+## Most important things to remember
+
+1. Three JS is a library that allows us to create 3D scenes in the browser
+2. Vite is used to create a dev server
+3. NVM can be used to change the version of node that you are using
+4. React Three Fibre is a library that allows us to use Three JS with React
+5. Poimandres is an organisation that built react three fibre.
+6. DREI can be used to find prebuilt components for three js
